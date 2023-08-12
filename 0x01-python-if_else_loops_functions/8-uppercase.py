649968eadd968a7d2963f34fde79e5f916f01beb
SisterToch <ibekwetestimony3@gmail.com>
2023-08-12 02:48:32 -0700

    to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in str:
+        if ord(i) >= ord(A) and ord(i) <= ord(Z):
+            i = chr(ord(i)
+        print("{:s}".format(i), end='')
+
+    print('\n', end="")
