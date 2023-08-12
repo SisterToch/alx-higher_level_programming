1c56eab174a34c20accf040b6df945b7b3522ff6
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-08-12 03:02:09 -0700

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(str):
     for i in str:
         if ord(i) >= ord(A) and ord(i) <= ord(Z):
-            i = chr(ord(i)
+            i = chr(ord(i))
         print("{:s}".format(i), end='')
 
     print('\n', end="")
