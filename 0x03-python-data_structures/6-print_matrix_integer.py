eee4e2d9dd59f7ab7b23abaf2292c52a5781954c
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-08-14 05:49:19 -0700

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
+    for i in matrix:
+        for j in row:
+            print("{:d}".format(j), end=" ")
         print()
