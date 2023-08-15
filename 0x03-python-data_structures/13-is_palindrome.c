391d8060c34cf87d9104e48f8ded367e2f3bf3b3
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-08-15 04:20:02 -0700

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,6 +8,9 @@
   *
   * Return: 0 if it is not a palindrome, 1 if it is a palindrome
   */
+size_t listint_len(const listint_t *h);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+
 int is_palindrome(listint_t **head)
 {
     listint_t *start = NULL, *end = NULL;
@@ -36,4 +39,48 @@ int is_palindrome(listint_t **head)
     return (1);
 }
 
- 
+/**
+  * get_nodeint_at_index - Gets a node from a linked list
+  * @head: The head of the linked list
+  * @index: The index to find in the linked list
+  *
+  * Return: The specific node of the linked list
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current = head;
+	unsigned int iter_times = 0;
+
+	if (head)
+	{
+		while (current != NULL)
+		{
+			if (iter_times == index)
+				return (current);
+
+			current = current->next;
+			++iter_times;
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+  * slistint_len - Counts the number of elements in a linked list
+  * @h: The linked list to count
+  *
+  * Return: Number of elements in the linked list
+  */
+size_t listint_len(const listint_t *h)
+{
+	int lenght = 0;
+
+	while (h != NULL)
+	{
+		++lenght;
+		h = h->next;
+	}
+
+	return (lenght);
+} 
