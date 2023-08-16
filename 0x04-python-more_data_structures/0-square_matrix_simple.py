193dbfdba5113f7d5826baf768c6aa78f6e170c5
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-08-16 12:08:03 -0700

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda j: j * j, row)) for row in matrix]
+    return [list(map(lambda j: j * j, i)) for i in matrix]
