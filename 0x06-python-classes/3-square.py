fd4893fbc50b6bbfe4018cc616bad056b53eb2f0
SisterToch <ibekwetestimony3@gmail.com>
2023-09-02 09:20:53 -0700

    find the area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""this is a square class"""
+
+
+class Square:
+    """this is an empty square"""
+
+    def __init__(self, size=0):
+        """it however has the size instance"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise("size must be >=0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculate the area using the size"""
+        return (self.__size ** 2) 
