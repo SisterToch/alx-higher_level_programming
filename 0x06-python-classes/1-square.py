9081e99d06ca97cbb6f08d2b68440c536b622e62
SisterToch <ibekwetestimony3@gmail.com>
2023-09-02 08:43:50 -0700

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,5 @@ class Square:
     """this is an empty square"""
 
     def __init__(self, size):
-         """it however has the size instance"""
+        """it however has the size instance"""
         self.__size = size
