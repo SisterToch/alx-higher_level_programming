27d1b34a512a5d213fe69744b40278790e702fd2
SisterToch <ibekwetestimony3@gmail.com>
2023-09-12 03:07:19 -0700

    inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""checks if object is an instance of a class that
+inherited from the specified class or not
+"""
+
+
+def inherits_from(obj, a_class):
+    """Returnsrue is object is subinstance
+    (directly or indirectly) from the specified class; otherwise False
+    """
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
