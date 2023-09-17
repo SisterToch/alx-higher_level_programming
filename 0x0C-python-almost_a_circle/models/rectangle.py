bcbc9e5675c652888575c28bd99749650900cc13
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-09-17 08:06:11 -0700

    add

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,8 +72,10 @@ class Rectangle(Base):
         return (self.__height * self.__width)
 
     def display(self):
+        for i in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
-        return ("[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
