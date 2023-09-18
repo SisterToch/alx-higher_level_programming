98c6067185b2a5490eba63edaa309b932d0a9902
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-09-18 02:07:22 -0700

    positional

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,26 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        if args:
+            square_arg = len(args)
+            if square_arg >= 1:
+                self.id = args[0]
+            if square_arg >= 2:
+                self.size = args[1]
+            if square_arg >= 3:
+                self.x = args[2]
+            if square_arg >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'size':
+                    self.size = value
+                elif key == 'x':
+                    self.x = value
+                elif key == 'y':
+                    self.y = value
+            
