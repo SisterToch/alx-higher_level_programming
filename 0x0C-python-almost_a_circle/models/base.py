cad3134b1656cfd79e9caac53e8ad40776a177a2
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-09-18 03:34:41 -0700

    added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+import json
 
 class Base:
     """the base of class"""
@@ -13,3 +13,10 @@ class Base:
         else:
             Base.__nb_objects +=1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the json strin representation of list"""
+        if list_dictionaries is None or []:
+            return "[]"
+        return json.dumps(list_dictionaries)
