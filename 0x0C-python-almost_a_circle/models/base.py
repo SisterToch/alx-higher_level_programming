c87770273d583bb02b36cc69ffa416127566cb53
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-09-18 05:05:00 -0700

    added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,15 @@ class Base:
         if json_string is None or len(json_string) == 0:
             json_string = []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """another instance"""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+        else:
+            raise ValueError("Unsupported class")
+        dummy_instance.update(**dictionary)
+        return dummy_instance
