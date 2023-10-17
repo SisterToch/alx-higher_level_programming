0ff8d7fcb7ead0acd4f027cbb36f778a5122c7e6
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-10-17 16:33:31 -0700

    Ad

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,1 @@
---create the table if it doesnt exist
---table is first_table
-CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
